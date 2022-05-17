e6f3427b16b10adac36000ad4c34312b5ee8368b
proffancs <proffancs@users.noreply.github.com>
2022-05-17 19:21:29 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ echo "...OTP IMAGEBUILDER... install & configure packages"
 apt-get install -y \
     apt-utils apt-transport-https ca-certificates software-properties-common build-essential sudo gnupg \
     curl wget git netcat netcat-openbsd zip unzip file gcc lsb-release \
-    gnupg-utils gpg openssl readline-common less libcurl4 libssl1.1 libffi7 jq \
+    gnupg-utils gpg readline-common less libcurl4 libssl1.1 libffi7 jq \
     software-properties-common debconf-utils dos2unix \
     libunwind8 libicu66 tzdata python-is-python3 python3-pip \
     locales language-pack-hu language-pack-en language-pack-en-base language-pack-hu-base fonts-dejavu ttf-dejavu

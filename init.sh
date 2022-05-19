77a8c0db34cfd0056a4e4f532b47ba36467473b7
proffancs <proffancs@users.noreply.github.com>
2022-05-19 09:57:26 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -28,7 +28,7 @@ apt-get update -y
 apt-get install -y helm
 echo "...OTP EBIZ COMMON IMAGEBUILDER... install azure cli..."
 curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ focal main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ bionic main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
 apt-get update -y
 apt-get install -y azure-cli
 echo "...OTP EBIZ COMMON IMAGEBUILDER... install kubelogin"

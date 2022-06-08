957d6d77a322a4648aafbe921c7670a05e04a8fb
proffancs <proffancs@users.noreply.github.com>
2022-06-08 11:53:27 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -17,17 +17,17 @@ echo "...OTP EBIZ COMMON IMAGEBUILDER... update certificates"
 update-ca-certificates
 echo "...OTP EBIZ COMMON IMAGEBUILDER... install kube*"
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update -y
 apt-get install -y kubelet kubeadm kubectl
 echo "...OTP EBIZ COMMON IMAGEBUILDER... install helm"
-curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+curl https://baltocdn.com/helm/signing.asc | apt-key add -
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | tee /etc/apt/sources.list.d/helm-stable-debian.list
 apt-get update -y
 apt-get install -y helm
 echo "...OTP EBIZ COMMON IMAGEBUILDER... install azure cli..."
-curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ bionic main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ bionic main" | tee /etc/apt/sources.list.d/azure-cli.list
 apt-get update -y
 apt-get install -y azure-cli
 echo "...OTP EBIZ COMMON IMAGEBUILDER... install kubelogin"
@@ -48,22 +48,27 @@ cd ..
 rm -rf azcopy
 chmod gou+x /usr/bin/azcopy
 echo "...OTP EBIZ COMMON IMAGEBUILDER... install github cli"
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 apt-get update -y
 apt-get install -y gh
 echo "...OTP EBIZ COMMON IMAGEBUILDER... install docker"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu bionic stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu bionic stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update -y
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 echo "...OTP EBIZ COMMON IMAGEBUILDER... install nodejs, npm"
 curl -sL https://deb.nodesource.com/setup_${VERSION_NODE}.x | bash -
 apt-get install -y nodejs
+echo "...OTP EBIZ COMMON IMAGEBUILDER... install yarn"
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get update
+apt-get install -y yarn
 echo "...OTP EBIZ COMMON IMAGEBUILDER... install angular"
 echo N | npm install -g @angular/cli@${VERSION_ANGULAR}
 echo "...OTP EBIZ COMMON IMAGEBUILDER... install java"
-apt-get install -y openjdk-${VERSION_JDK}-jdk
+apt-get install -y ca-certificates-java openjdk-${VERSION_JDK}-jdk default-jre
 echo "...OTP EBIZ COMMON IMAGEBUILDER... install maven"
 mkdir -p /usr/share/maven /usr/share/maven/ref
 curl -fsSL -o /tmp/apache-maven.tar.gz ${URL_MAVEN}/apache-maven-${VERSION_MAVEN}-bin.tar.gz
